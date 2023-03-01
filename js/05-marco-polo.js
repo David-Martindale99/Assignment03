94b64a07837665edf23ac837a60ede3591433f03
DAVID MARTINDALE <dlmartindale99@gmail.com>
2023-02-28 18:03:00 -0800

    Marco Polo logic fixed

--- js/05-marco-polo.js
+++ js/05-marco-polo.js
@@ -3,15 +3,13 @@ let numCall
 
 //Basic F&B or M&P...for loop w/ nested if, variable throws up to 100...evaluates with % to find 3 and 5
 //multiples to then log them based on our criteria...blah blah blah
-for (numCall = 0; numCall <= 100; numCall++) {
-    if (numCall === 0) {
-        console.log(0)
-    } else if (numCall % 3 === 0 && numCall % 5 === 0) {
-        console.log('FizzBuzz')
+for (numCall = 1; numCall <= 100; numCall++) {
+    if (numCall % 3 === 0 && numCall % 5 === 0) {
+        console.log('Marco! Polo!')
     } else if (numCall % 3 === 0) {
-        console.log('Fizz')
+        console.log('Marco!')
     } else if (numCall % 5 === 0) {
-        console.log('Buzz')
+        console.log('Polo!')
     } else {
         console.log(numCall)
     }
